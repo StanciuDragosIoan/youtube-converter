7e73cf055adfd67cbfad07bbdde1b6ebe1585c39
stanciudragosioan <stanciudragosioan92@gmail.com>
2023-03-30 13:30:18 +0300

    update project and dependencies

--- server.js
+++ server.js
@@ -1,55 +1,10 @@
 const ytdl = require("ytdl-core");
 const fs = require("fs");
 const { readdirSync, unlinkSync, existsSync, mkdirSync } = fs;
-const { promisify } = require("util");
-const path = require("path");
 const fastifyCron = require("fastify-cron");
 
-// Require the framework and instantiate it
-const fastify = require("fastify")({
-  logger: {
-    prettyPrint: {
-      translateTime: true,
-      ignore: "pid,hostname,reqId,responseTime,req,res",
-      messageFormat: "{msg} [id={reqId} {req.method} {req.url}]",
-    },
-  },
-});
 
-//set up cron
-fastify.register(fastifyCron, {
-  jobs: [
-    {
-      // Only these two properties are required,
-      // the rest is from the node-cron API:
-      // https://github.com/kelektiv/node-cron#api
-      cronTime: "0 0 * * *", // Everyday at midnight UTC
-
-      // Note: the callbacks (onTick & onComplete) take the server
-      // as an argument, as opposed to nothing in the node-cron API:
-      onTick: async () => {
-        const dir = "./downloads";
-        readdirSync(dir).forEach((f) => unlinkSync(`${dir}/${f}`));
-      },
-    },
-  ],
-});
-
-//create downloads dir if it does not exist
-const downloadsDir = "./downloads";
-if (!existsSync(downloadsDir)) {
-  mkdirSync(downloadsDir);
-}
-
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "/downloads"),
-});
-
-fastify.register(require("point-of-view"), {
-  engine: {
-    ejs: require("ejs"),
-  },
-});
+const { promisify } = require("util");
 
 const downloadContentAndSendLink = async (srcUrl, ext, reply, opts = {}) => {
   const videoID = ytdl.getVideoID(srcUrl);
@@ -64,7 +19,7 @@ const downloadContentAndSendLink = async (srcUrl, ext, reply, opts = {}) => {
   }
 
   ytdl(srcUrl, opts)
-    .pipe(fs.createWriteStream(`./downloads/${videoID}.${ext}`))
+    .pipe(fs.createWriteStream(`./templates/downloads/${videoID}.${ext}`))
     .on("finish", async function () {
       return reply.send({ id: videoID, title, ext, isTooLong });
     });
@@ -72,9 +27,67 @@ const downloadContentAndSendLink = async (srcUrl, ext, reply, opts = {}) => {
 
 const pDownloadAndSend = promisify(downloadContentAndSendLink);
 
+
+
+//logging config
+const envToLogger = {
+  development: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
+
+const environment = "development";
+
+
+// Require the framework and instantiate it
+const fastify = require("fastify")({
+  logger: envToLogger[environment]
+});
+
+//create downloads dir if it does not exist
+const downloadsDir = "./templates/downloads";
+if (!existsSync(downloadsDir)) {
+  mkdirSync(downloadsDir);
+}
+
+
+//register plugins
+fastify.register(require("@fastify/static"), {
+  root: require("path").join(__dirname, "templates"),
+});
+
+//set up cron
+fastify.register(fastifyCron, {
+  jobs: [
+    {
+      // Only these two properties are required,
+      // the rest is from the node-cron API:
+      // https://github.com/kelektiv/node-cron#api
+      cronTime: "0 0 * * *", // Everyday at midnight UTC
+
+      // Note: the callbacks (onTick & onComplete) take the server
+      // as an argument, as opposed to nothing in the node-cron API:
+      onTick: async () => {
+        const dir = "./templates/downloads";
+        readdirSync(dir).forEach((f) => unlinkSync(`${dir}/${f}`));
+      },
+    },
+  ],
+});
+
+
+
 // Declare a route
 fastify.get("/", (req, reply) => {
-  reply.view("/templates/index.ejs");
+  reply.sendFile("index.html");
 });
 
 fastify.post("/audio", async (request, reply) => {
@@ -88,9 +101,9 @@ fastify.post("/audio", async (request, reply) => {
 
 fastify.post("/video", async (req, reply) => {
   const srcUrl = req.body.url;
-
   await pDownloadAndSend(srcUrl, "mp4", reply);
 });
+
 
 fastify.get("/download/:id", async (request, reply) => {
   const { id } = request.params;
@@ -99,23 +112,31 @@ fastify.get("/download/:id", async (request, reply) => {
   const { title } = videoDetails;
   const { extension } = request.query;
 
-  //delete downloaded file after 1 min
-  setTimeout(() => {
-    const dir = "./downloads";
 
+  //delete downloaded file after 1 min
+  setTimeout(async () => {
+    const dir = "./templates/downloads";
     readdirSync(dir).forEach((f) =>
       f === `${id}.${extension}` ? unlinkSync(`${dir}/${f}`) : null
     );
   }, 60000);
 
-  return reply.download(`/${id}.${extension}`, `${title}.${extension}`);
+  return reply.download(
+    `./downloads/${id}.${extension}`,
+    `${title}.${extension}`
+  );
 });
+
+
+
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000);
-    fastify.cron.startAllJobs(); //start cron
+    await fastify.listen({ port: 3000 }, () => {
+      fastify.cron.startAllJobs();
+    });
+
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
